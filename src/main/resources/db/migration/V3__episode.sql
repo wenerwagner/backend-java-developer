bd4de673d9e0e7791f58e0aa072176e3c740ce77
Wener Castro <wenerwagner@gmail.com>
2026-04-15 15:50:13 -0300

    🗃️ add relation between show and episode (#8)

--- src/main/resources/db/migration/V3__episode.sql
+++ src/main/resources/db/migration/V3__episode.sql
@@ -10,7 +10,11 @@ CREATE TABLE episode
     air_stamp      TIMESTAMP WITHOUT TIME ZONE,
     rating         DECIMAL(5, 2),
     summary        VARCHAR(255),
+    fk_show        VARCHAR(255)             NOT NULL,
     created_at     TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at     TIMESTAMP WITH TIME ZONE NOT NULL,
     CONSTRAINT pk_episode PRIMARY KEY (id)
 );
+
+ALTER TABLE episode
+    ADD CONSTRAINT FK_EPISODE_ON_FK_SHOW FOREIGN KEY (fk_show) REFERENCES show (id);
