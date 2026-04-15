6b781840764de12662001153dc650992a1095f34
Wener Castro <wenerwagner@gmail.com>
2026-04-15 09:52:33 -0300

    remove debbuging code

--- src/main/java/com/cmanager/app/authentication/security/SecurityConfig.java
+++ src/main/java/com/cmanager/app/authentication/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/auth/login").permitAll()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 )
                 .authenticationProvider(daoAuthProvider)
                 .oauth2ResourceServer(oauth2 -> oauth2
